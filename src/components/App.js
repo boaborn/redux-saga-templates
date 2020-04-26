4ea897c6cb1d8c2cbafabe924f8705e5e53017c3
Michael <michaelzhaojq@hotmail.com>
2020-04-26 22:38:19 +1200

    setup

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function App() {
   return (
-    <div className>
-      app
+    <div>
+      test
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
 import * as serviceWorker from './serviceWorker'
+import reducers from './reducers'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+import axios from 'axios'
+
+axios.defaults.withCredentials = true
+axios.defaults.baseUrl = 'https://rem-rest-api.herokuapp.com/api'
+
+const store = createStore(reducers)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={ store }>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 )
 
